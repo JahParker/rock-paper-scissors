745971b20d2296d1b6d78c22fea34cd82d581d0a
JahParker <63827876+JahParker@users.noreply.github.com>
2023-12-22 22:56:25 -0500

    Add buttons with game functionality

--- script.js
+++ script.js
@@ -5,49 +5,54 @@ function getComputerChoice() {
 }
 
 // Don't optimize. Just see it through
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+  const result = document.querySelector('#result')
+  let computerSelection = getComputerChoice()
+  
   switch (playerSelection.toLowerCase()) {
     case 'rock':
       if (computerSelection == 'scissors') {
-        return `You win! :) ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+        result.textContent = `You win! :) ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
       } else if (computerSelection == 'paper') {
-        return `You lose! :( ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`
+        result.textContent = `You lose! :( ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`
       } else {
-        return `It's a draw! ${playerSelection.toUpperCase()} == ${computerSelection.toUpperCase()}`
+        result.textContent = `It's a draw! ${playerSelection.toUpperCase()} == ${computerSelection.toUpperCase()}`
       }
       break;
     case 'paper':
       if (computerSelection == 'rock') {
-        return `You win! :) ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+        result.textContent = `You win! :) ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
       } else if (computerSelection == 'scissors') {
-        return `You lose! :( ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`
+        result.textContent = `You lose! :( ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`
       } else {
-        return `It's a draw! ${playerSelection.toUpperCase()} == ${computerSelection.toUpperCase()}`
+        result.textContent = `It's a draw! ${playerSelection.toUpperCase()} == ${computerSelection.toUpperCase()}`
       }
       break;
     case 'scissors':
       if (computerSelection == 'paper') {
-        return `You win! :) ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
+        result.textContent = `You win! :) ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}`
       } else if (computerSelection == 'rock') {
-        return `You lose! :( ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`
+        result.textContent = `You lose! :( ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`
       } else {
-        return `It's a draw! ${playerSelection.toUpperCase()} == ${computerSelection.toUpperCase()}`
+        result.textContent = `It's a draw! ${playerSelection.toUpperCase()} == ${computerSelection.toUpperCase()}`
       }
       break;
     default:
-      return "Uh.. Lets try this again. Type rock, paper, or scissors"
+      result.textContent = "Uh.. Lets try this again. Type rock, paper, or scissors"
       break;
   }
 }
 
+function test() {
+  console.log("this is a test");
+}
+
 function game() {
-  let computerChoice;
-  let playerChoice;
-  for (i = 0; i < 5; i++) {
-    playerChoice = prompt("Type rock, paper, or scissors", "");
-    computerChoice = getComputerChoice();
-    console.log(playRound(playerChoice, computerChoice));
-  }
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach((button) => {
+    const playerChoice = button.value;
+    button.addEventListener('click', () => {playRound(playerChoice)});
+  });
 }
 
 game()
